0aef1df2ce6f4d5a3a641380320a16108aa616d5
BiancaSozzi <biancasozzi@gmail.com>
2020-08-13 14:43:18 +0200

    wip

--- src/main/java/com/spring/course/springit/SpringitApplication.java
+++ src/main/java/com/spring/course/springit/SpringitApplication.java
@@ -12,18 +12,18 @@ import org.springframework.context.annotation.Bean;
 @EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
 
-	@Autowired
-	private SpringitProperties springitProperties;
+//	@Autowired
+//	private SpringitProperties springitProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-			System.out.println("Welcome Message: " + springitProperties.getWelcomeMsg());
-		};
-	}
+//	@Bean
+//	CommandLineRunner runner() {
+//		return args -> {
+//			System.out.println("Welcome Message: " + springitProperties.getWelcomeMsg());
+//		};
+//	}
 
 }

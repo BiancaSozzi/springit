f40f54c52b4a5c7f4dd5866bd71c43e6444ee1ed
BiancaSozzi <biancasozzi@gmail.com>
2020-08-19 09:41:48 +0200

    comment DB initialization

--- src/main/java/com/spring/course/springit/SpringitApplication.java
+++ src/main/java/com/spring/course/springit/SpringitApplication.java
@@ -25,7 +25,7 @@ public class SpringitApplication {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-	@Bean
+//	@Bean
 	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
 		return args -> {
 //			System.out.println("Welcome Message: " + springitProperties.getWelcomeMsg());

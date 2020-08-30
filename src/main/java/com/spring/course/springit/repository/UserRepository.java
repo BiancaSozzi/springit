ab1e33681f8bfa2f414c3d782cc521ad7558e351
BiancaSozzi <biancasozzi@gmail.com>
2020-08-30 09:45:35 +0200

    Add UserDetailsService implementation

--- src/main/java/com/spring/course/springit/repository/UserRepository.java
+++ src/main/java/com/spring/course/springit/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.spring.course.springit.repository;
 import com.spring.course.springit.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+
 }

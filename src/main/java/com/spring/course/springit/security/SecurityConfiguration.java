c4f8719276261338144bbb5b22a694f15b208a06
BiancaSozzi <biancasozzi@gmail.com>
2020-08-30 10:25:43 +0200

    add configuration

--- src/main/java/com/spring/course/springit/security/SecurityConfiguration.java
+++ src/main/java/com/spring/course/springit/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.spring.course.springit.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+				.requestMatchers(EndpointRequest.to("info")).permitAll()
+				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+				.antMatchers("/actuator/").hasRole("ADMIN")
 				.antMatchers("/").permitAll()
 				.antMatchers("/link/submit").hasRole("USER")
 //				.anyRequest().authenticated()
@@ -28,6 +32,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.and()
 			.httpBasic();
 	}
+
+	//EndpointRequest (to match the endpoints)
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {

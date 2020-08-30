a7434e64e078f34d08da4ffe71cee053bf9b83bd
BiancaSozzi <biancasozzi@gmail.com>
2020-08-30 14:01:51 +0200

    add springboot thymeleaf security

--- src/main/java/com/spring/course/springit/SpringitApplication.java
+++ src/main/java/com/spring/course/springit/SpringitApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication (scanBasePackages = {"com.spring", "com.github.timpeeters.boot.shutdown"})
 @EnableConfigurationProperties(SpringitProperties.class)
@@ -42,4 +43,10 @@ public class SpringitApplication {
 		return new PrettyTime();
 	}
 
+	// TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+	// TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
+	}
 }

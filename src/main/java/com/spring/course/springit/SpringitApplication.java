5b9e16999b1af7a43f7afcc5572d1ef7bf9e759f
BiancaSozzi <biancasozzi@gmail.com>
2020-09-11 11:16:58 +0200

    transactional

--- src/main/java/com/spring/course/springit/SpringitApplication.java
+++ src/main/java/com/spring/course/springit/SpringitApplication.java
@@ -11,11 +11,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication (scanBasePackages = {"com.spring", "com.github.timpeeters.boot.shutdown"})
 @EnableConfigurationProperties(SpringitProperties.class)
+@EnableTransactionManagement // to allow handling transactions with @Transaction
 public class SpringitApplication {
 
 //	@Autowired
--- src/main/java/com/spring/course/springit/service/UserService.java
+++ src/main/java/com/spring/course/springit/service/UserService.java
@@ -5,6 +5,7 @@ import com.spring.course.springit.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -22,5 +23,13 @@ public class UserService {
 
 	public User save(User user) {
 		return  userRepository.save(user);
+	}
+
+	@Transactional // will save all users in the list or none. To use it enable it by using @EnableTransactionManagement
+	public void saveUsers(User... users) {
+		for (User user: users) {
+			logger.info("Saving User: " + user.getEmail());
+			userRepository.save(user);
+		}
 	}
 }
